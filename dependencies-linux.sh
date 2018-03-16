a91e17c7e5dd1fb44ee4f53a6bd96d1bda9d5fcc
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-16 01:21:48 -0400

    Update dependencies-linux.sh

--- dependencies-linux.sh
+++ dependencies-linux.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 apt-get install build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
+echo "Dependencies installation complete"
+make
+echo "Mynt Build process complete"
